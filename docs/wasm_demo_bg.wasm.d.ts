e38f14cef360e9caca72c763fdd42d02b293fdf1
Parth Shitole <79759549+ParthNS1@users.noreply.github.com>
2025-07-03 22:52:43 +0530

    Add files via upload

---
+++ docs/wasm_demo_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const heavy_fib_task: (a: number) => bigint;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_start: () => void;
