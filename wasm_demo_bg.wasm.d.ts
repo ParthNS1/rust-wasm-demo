85704c2951b420d4e97b5c7925e505ffcc3fff12
Parth Shitole <79759549+ParthNS1@users.noreply.github.com>
2025-07-04 10:40:04 +0530

    Delete wasm_demo_bg.wasm.d.ts

--- wasm_demo_bg.wasm.d.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export const heavy_fib_task: (a: number) => bigint;
-export const __wbindgen_export_0: WebAssembly.Table;
-export const __wbindgen_start: () => void;
